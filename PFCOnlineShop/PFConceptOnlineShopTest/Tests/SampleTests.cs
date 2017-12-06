7149a67315d62b9a90341176a9dc056b0a52aa93
Anita Mak <anita.c.mak.28@gmail.com>
2017-12-06 14:06:45 +0100

    PFCOnlineShop

---
+++ PFCOnlineShop/PFConceptOnlineShopTest/Tests/SampleTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace PFConceptOnlineShopTest.Tests
+{
+    [TestClass]
+    public class SampleTests
+    {
+        private IWebDriver _driver;
+
+        [TestInitialize]
+        public void InitializeBrowser()
+        {
+           
+        }
+
+      
+        
+            
+        }
+
+    }
+
+
